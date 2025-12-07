38e0a36c7375e88d417bb30165d07f482a007384
rekha059-arch <rekha059@gmail.com>
2025-12-07 22:31:36 +0530

    Update hosting.py

--- tourism_project/hosting/hosting.py
+++ tourism_project/hosting/hosting.py
@@ -4,7 +4,7 @@ import os
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="tourism_project/deployment",     # the local folder containing your files
-    repo_id="rekhchan/MLoPs",                               # the target repo
+    repo_id="https://huggingface.co/spaces/rekhchan/MLoPs",                               # the target repo
     repo_type="space",                            # dataset, model, or space
     path_in_repo="",                              # optional: subfolder path inside the repo
 )
