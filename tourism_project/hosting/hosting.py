9cc02835a14d63a1adf6589265da290f8f808b8d
rekha059-arch <rekha059@gmail.com>
2025-12-07 14:21:27 +0000

    first commit

--- tourism_project/hosting/hosting.py
+++ tourism_project/hosting/hosting.py
@@ -1,7 +1,7 @@
 from huggingface_hub import HfApi
 import os
 
-api = HfApi(token=os.getenv("RekhaChandrappa"))
+api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="tourism_project/deployment",     # the local folder containing your files
     repo_id="MLoP",                               # the target repo
